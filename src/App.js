bf2045a49a342a41b99981d9656d2617e9ad989c
Vivvie Lin <vvlnote@hotmail.com>
2019-01-26 10:51:18 -0800

    freeCodeCamp-Front End Libraries Projects: Markdown Previewer, still failed on 2 testes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
+
+const Editor = (props) => (
+
+    <div>
+    <h3>Markdown Editor</h3>
+    <textarea id="editor" onChange={props.onChange} value={props.value}></textarea>
+    </div>
+  );
+
+const MarkdownPreview = (props) => (
+    <div>
+    <h3>Mrakdown Preview</h3>
+    <div id="preview" dangerouslySetInnerHTML={props.markup}>
+    </div>
+    </div>
+);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        markdown: 
+'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Google](http://google.com)*'
+      };
+      this.markdownEditorUpdate = this.markdownEditorUpdate.bind(this);
+  }
+  createMarkup() {
+    var options = {sanitize: true};
+    return {__html:marked(this.state.markdown, options)};
+  }
+
+  markdownEditorUpdate(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+    console.log(this.state.markdown);
+  }
   render() {
+    console.log(this.state.markdown);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Editor value={this.state.markdown} onChange={this.markdownEditorUpdate} />
+        <MarkdownPreview markup={this.createMarkup()}/>
       </div>
     );
   }
